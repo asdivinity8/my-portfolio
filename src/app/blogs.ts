7e6f0ba2ca31cd48f6625cebd46e8ce592eb0a1a
Ankit Verma <ankit.verma@imaginea.com>
2020-05-13 21:54:01 +0530

    added new blog content

--- src/app/blogs.ts
+++ src/app/blogs.ts
@@ -5,5 +5,12 @@ export const BLOGS = [
         imageUrl: "assets/promise.png",
         url: "https://medium.com/@asdivinity8/javascript-async-await-in-and-out-1dfbdf7fb0c1?source=friends_link&sk=0a82b0ee4784cc4b28b58334a4d2334f",
         target: "_blank"
+    },
+    {
+        name: "Angular Material Menu",
+        description: "Angular material library really makes it easy to build beautiful single page applications with Angular. It provides a wise variety to commonly used elements like tables, forms, menus, navigation, sliders, buttons, etc.",
+        imageUrl: "assets/material-logo.png",
+        url: "https://medium.com/@asdivinity8/passing-data-to-angular-material-menu-485e38012c?source=friends_link&sk=70798c05a5ff7590365cdeee790602b2",
+        target: "_blank"
     }
 ]
